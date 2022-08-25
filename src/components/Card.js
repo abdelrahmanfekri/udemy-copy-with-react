33ca6a8934b6a583fbc6c27d5e9ffb693731b09b
abdelrahman <abdelrahmanfakre@gmail.com>
2022-08-25 22:25:22 +0200

    fix : solve issues

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactStars from 'react-rating-stars-component';
-import '../index.css';
+import style from '../index.module.css';
 function Card(props) {
+    const {image,title,headline,rating,price} = props.course;
     return (
-        <div className="courses-content">
-            <img src={props.course.image} alt="course Logo" width="100%" />
+        <div className={style.coursesContent}>
+            <img src={image} alt="course Logo" width="100%" />
             <figcaption>
-                <b> ${props.course.title} </b>
+                <b> ${title} </b>
             </figcaption>
-            <p style={{ fontSize: "12px" }}>{props.course.headline}</p>
+            <p style={{ fontSize: "12px" }}>{headline}</p>
             <div style={{ color: "darkorange",display:"flex",alignItems:"center" }}>
-                <span style={{ fontSize: "20px", color: "rgb(248, 165, 23)" }}>{parseFloat(props.course.rating).toFixed(1)}
+                <span style={{ fontSize: "20px", color: "rgb(248, 165, 23)" }}>{parseFloat(rating).toFixed(1)}
                 </span>
-                <ReactStars count={props.course.rating} size={24}color="#ffd700" edit={false}></ReactStars>
+                <ReactStars count={rating} size={24}color="#ffd700" edit={false}></ReactStars>
                 <span style={{ color: "darkgray" }}>(56665)</span>
             </div>
             <div>
                 <span> price :</span>
-                <b id="course-price">${props.course.price}</b>
+                <b id="course-price">${price}</b>
             </div>
         </div >
     );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import style from './index.module.css';
 import Card from './components/Card.js';
 import courses from './courses.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <section className='courses' style={{margin:"20px"}}>
+  <section className={style.courses} style={{margin:"20px"}}>
     <h3>Expand your career opportunities with Python</h3>
     <p>
       Take one of Udemy's range of Python courses and learn how to code
@@ -15,8 +15,8 @@ root.render(
       machine learning. You'll learn how to build everything from games to
       sites to apps. Choose from a range of courses that will appeal to..
     </p>
-    <button id="explore-btn" style={{ width: "150px" }}><b>Explore Python</b></button>
-    <div id="display-courses">
+    <button className={style.exploreBtn} id="explore-btn" ><b>Explore Python</b></button>
+    <div id="display-courses" className={style.displayCourses}>
       {courses.map((course) => <Card key={course.id} course={course} />)}
     </div>
   </section>
